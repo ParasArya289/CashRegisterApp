70633eefeb7a7a7ffc1fd4037bc12b7a58dca4d0
Paras Arya <82272875+ParasArya289@users.noreply.github.com>
2022-12-22 00:11:58 +0530

    Initial commit

---
+++ src/cashRegister.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+let CashRegister = () => {
+  let [billAmount, setBillAmount] = useState(0);
+  let [cash, setCash] = useState(0);
+  //temp soluton: init state arr to resolve table rendering without td as program maps td and doesnt has html structure.
+  let [numOfNotes, setNumOfNotes] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
+  let notesArray = [2000, 500, 100, 50, 20, 10, 5, 1];
+  let amountToBeReturn = cash - billAmount;
+  let numberOfNotes = [];
+
+  let returnHandler = () => {
+    for (let i = 0; i < notesArray.length; i++) {
+      let j = Math.trunc(amountToBeReturn / notesArray[i]);
+
+      numberOfNotes.push(j);
+
+      amountToBeReturn %= notesArray[i];
+
+      setNumOfNotes(numberOfNotes);
+    }
+    console.log(numOfNotes);
+  };
+
+  return (
+    <div className="container">
+      <h1>Cash Register</h1>
+      <p>Enter Bill amount and Cash given by User</p>
+      <h3>Bill Amount</h3>
+      <input
+        type="number"
+        onChange={(e) => {
+          setBillAmount(e.target.value);
+        }}
+      />
+      <h3>Cash</h3>
+      <input
+        type="number"
+        onChange={(e) => {
+          setCash(e.target.value);
+        }}
+      />
+      <button onClick={returnHandler}>Submit</button>
+      {amountToBeReturn > 0 && <h2>To be return: {amountToBeReturn} </h2>}
+      <table style={{ boder: "1px" }}>
+        <tr>
+          <th>Return</th>
+          {numOfNotes.map((el) => {
+            return <td>{el}</td>;
+          })}
+        </tr>
+        <tr>
+          <th>Notes</th>
+          {notesArray.map((el) => {
+            return <td>{el}</td>;
+          })}
+        </tr>
+      </table>
+    </div>
+  );
+};
+export default CashRegister;
